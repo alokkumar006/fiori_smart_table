fdad9ec3d2a2843f4f60394614b7d576bf4bda68
Alok Kumar <136841670+alokkumar006@users.noreply.github.com>
2025-05-14 15:48:52 +0530

    added webapp folder

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/gyansys/smarttable/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/gyansys/smarttable/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
